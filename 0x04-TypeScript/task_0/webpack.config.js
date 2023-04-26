ee4ead191e086290a1bceccb9ac77cd015b8125d
Mohamed Suliman <sulimmoh@gmail.com>
2023-04-26 17:08:27 -0400

    Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -0,0 +1,30 @@
+ool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development"
+    })
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
