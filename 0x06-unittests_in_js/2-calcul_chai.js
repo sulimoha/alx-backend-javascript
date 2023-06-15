ff22857d2dde4af5a40ba2c9480e29a600342fef
Mohamed Suliman <sulimmoh@gmail.com>
2023-06-15 19:42:16 -0400

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,30 @@
+function calculateNumber(type, a, b) {
+    if (typeof a === 'undefined' || typeof b === 'undefined' || typeof type === 'undefined') {
+      throw new Error('Missing arguments');
+    }
+  
+    if (typeof type !== 'string') {
+      throw new TypeError('The type parameter must be a string');
+    }
+  
+    const arg1 = Math.round(a);
+    const arg2 = Math.round(b);
+  
+    if (Number.isNaN(arg1) || Number.isNaN(arg2)) {
+      throw new TypeError('The a and b parameters must be integers');
+    }
+  
+    switch (type) {
+      case 'SUM':
+        return arg1 + arg2;
+      case 'SUBTRACT':
+        return arg1 - arg2;
+      case 'DIVIDE':
+        return (arg2 === 0) ? 'Error' : arg1 / arg2;
+      default:
+        throw new Error('The type parameter only accept SUM, SUBTRACT, or DIVIDE');
+    }
+  }
+  
+  module.exports = calculateNumber;
+  
