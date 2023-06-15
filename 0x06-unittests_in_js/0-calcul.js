7dd1ddfc6d1cd325f6dfdcbf94d6c1f642b8e2f5
Mohamed Suliman <sulimmoh@gmail.com>
2023-06-15 19:36:49 -0400

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,17 @@
+function calculateNumber(a, b) {
+    if (typeof a === 'undefined' || typeof b === 'undefined') {
+      throw new Error('Missing arguments');
+    }
+  
+    const arg1 = Math.round(a);
+    const arg2 = Math.round(b);
+  
+    if (Number.isNaN(arg1) || Number.isNaN(arg2)) {
+      throw new TypeError('Parameters must be integers');
+    }
+  
+    return arg1 + arg2;
+  }
+  
+  module.exports = calculateNumber;
+  
